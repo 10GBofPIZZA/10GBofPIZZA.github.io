5f7ae060460572c027a0b61efb4c2c5768b445b3
kanekronos <64645260+KaneKronos@users.noreply.github.com>
2021-01-30 10:13:30 +0530

    js update

--- scripts.js
+++ scripts.js
@@ -3,20 +3,15 @@
 //index.html
 document.getElementById("myButton").onclick = function () 
 {
-    location.href = "Links.html";
+    location.href = './Links.html';
 };
 
 document.getElementById("games").onclick = function () 
 {
-    location.href = "Games.html";
+    location.href = 'Games.html';
 };
 
 document.getElementById("closeUI").onclick = function () 
 {
-    location.href = "index.html";
-};
-
-document.getElementById("closeUI").onclick = function () 
-{
-    location.href = "index.html";
+    location.href = 'index.html';
 };
