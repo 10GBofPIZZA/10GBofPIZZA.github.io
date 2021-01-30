7e419bb94a185ef1b852dea4dadcf0cd30e04be7
kanekronos <64645260+KaneKronos@users.noreply.github.com>
2021-01-30 09:56:25 +0530

    separate script file

---
+++ scripts.js
@@ -0,0 +1,17 @@
+//all the scripts, mostly the buttons
+
+//index.html
+document.getElementById("myButton").onclick = function () 
+{
+    location.href = "Links.html";
+};
+
+document.getElementById("games").onclick = function () 
+{
+    location.href = "Games.html";
+};
+
+document.getElementById("closeUI").onclick = function () 
+{
+    location.href = "index.html";
+};
