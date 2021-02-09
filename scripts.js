4ff70c49a5745d6b1b42cfff47285fa1bc4567bd
kanekronos <64645260+KaneKronos@users.noreply.github.com>
2021-02-09 07:30:58 +0530

    changing menus from buttons to lists

--- scripts.js
+++ scripts.js
@@ -10,3 +10,7 @@ document.getElementById("games").onclick = function ()
 {
     location.href = 'Games.html';
 };
+document.getElementById("closeUI").onclick = function () 
+{
+    location.href = 'index.html';
+};
