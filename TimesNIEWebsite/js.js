a400dcc58fcb19957f5a31330063f6ee44e4c2fa
kanekronos <64645260+KaneKronos@users.noreply.github.com>
2021-07-02 21:30:31 +0530

    final polish of lct timesnieproject

--- TimesNIEWebsite/js.js
+++ TimesNIEWebsite/js.js
@@ -1,23 +1,10 @@
-let times = [];
-        // example {id:1592304983049, title: 'Deadpool', year: 2015}
-        const addTime = (ev)=>{
-            ev.preventDefault();  //to stop the form submitting
-            let time = {
-                id: Date.now(),
-                Time: document.getElementById('time').value
-            }
-            times.push(time);
-            document.forms[0].reset(); // to clear the form for the next entries
-            //document.querySelector('form').reset();
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    console.log(bgColor);
+    document.body.style.backgroundColor = bgColor;
+}
 
-            //for display purposes only
-            console.warn('added' , {time} );
-            let pre = document.querySelector('#msg pre');
-            pre.textContent = '\n' + JSON.stringify(times, '\t', 2);
-
-            //saving to localStorage
-            localStorage.setItem('Times', JSON.stringify(times) );
-        }
-        document.addEventListener('DOMContentLoaded', ()=>{
-            document.getElementById('btn').addEventListener('click', addTime);
-});
+random_bg_color();
